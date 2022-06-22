61cf619129571697c47c6be579f24661c4c6c241
shubham <patilshubham2k21@gmail.com>
2022-06-22 11:39:46 +0530

    game finish

---
+++ src/components/SingleCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./singleCard.css";
+
+function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className="front" />
+        <img
+          src="/img/cover.png"
+          alt="card back"
+          className="back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
